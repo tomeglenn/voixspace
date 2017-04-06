4c77c4dde00c618876edf6ebd002d14e465bd122
Tom Glenn <tom.glenn@stormid.com>
2017-04-06 09:44:23 +0100

    Initial commit with threejs

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var twig = require('twig');
 var port = process.env.PORT || 1337;
 
+twig.cache(false);
+
+app.set('views', 'views');
+app.set('view engine', 'html');
+app.engine('html', twig.__express);
+
+app.use('/public', express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello, world!');
+  res.render('index');
+});
+
+app.get('/:id', function (req, res) {
+  res.render(req.params.id || 'index');
 });
 
 app.listen(port, function () {
