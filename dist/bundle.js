358fc3ec181f02535d3211605ac7447e853f5a79
Amandine Eap <amandine.eap@sinewavedesign.com>
2017-04-06 17:21:49 +0100

    remove test page

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,12 +63,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 108);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 108:
+/******/ ([
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -221,5 +220,4 @@ window.onload = function () {
 };
 
 /***/ })
-
-/******/ });
+/******/ ]);
