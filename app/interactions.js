7c75c38b8c7623134d61c1192bf35a332daacec9
Tom Glenn <tom.glenn@stormid.com>
2017-04-06 15:13:05 +0100

    Started implemented es6 classes etc

--- app/interactions.js
+++
@@ -1,4 +0,0 @@
-export default function clickNode(mesh) {
-  console.log('You clicked me');
-  mesh.material.color.setHex(0x0000ff);
-}
--- dist/bundle.js
+++ dist/bundle.js
@@ -927,47 +927,7 @@ process.umask = function() { return 0; };
 
 
 /***/ }),
-/* 8 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__interactions__ = __webpack_require__(29);
-/* harmony export (immutable) */ __webpack_exports__["a"] = cube;
-
-
-function cube() {
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
-  function render() {
-    requestAnimationFrame( render );
-
-    cube.rotation.x = Date.now() * 0.0005;
-    cube.rotation.y = Date.now() * 0.0001;
-
-    renderer.render( scene, camera );
-  }
-  render();
-
-  var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
-  domEvents.addEventListener(cube, 'click', function (event) {
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__interactions__["a" /* default */])(cube);
-  });
-}
-
-
-/***/ }),
+/* 8 */,
 /* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -979,18 +939,66 @@ module.exports = __webpack_require__(11);
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__cube__ = __webpack_require__(8);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__apiResource__ = __webpack_require__(31);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios__ = __webpack_require__(9);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_axios__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__forms__ = __webpack_require__(28);
 
 
+
+class App {
+  constructor() {
+    this.apiResource = new __WEBPACK_IMPORTED_MODULE_0__apiResource__["a" /* default */]();
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    this.camera.position.z = 5;
+  }
+
+  addCube() {
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+  }
+
+  render() {
+    requestAnimationFrame(this.render.bind(this));
+
+    this.cube.rotation.x = Date.now() * 0.0005;
+    this.cube.rotation.y = Date.now() * 0.0001;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  addToDom() {
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  registerDomEvents() {
+    var domEvents = new THREEx.DomEvents(this.camera, this.renderer.domElement);
+    domEvents.addEventListener(this.cube, 'click', function (event) {
+      event.target.material.color.setHex(0x0000ff);
+      this.apiResource.get();
+    }.bind(this));
+  }
+
+  init() {
+    this.addCube();
+    this.addToDom();
+    this.registerDomEvents();
+    this.render();
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["App"] = App;
 
 
 window.onload = function() {
-  __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__cube__["a" /* default */])();
-  __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__forms__["a" /* setupModals */])();
-}
+  var app = new App();
+  app.init();
+};
 
 
 /***/ }),
@@ -1831,56 +1839,47 @@ module.exports = function spread(callback) {
 
 
 /***/ }),
-/* 28 */
+/* 28 */,
+/* 29 */,
+/* 30 */,
+/* 31 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_axios__ = __webpack_require__(9);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_axios__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return setupModals; });
 
 
 const ENDPOINTS = {
   MESSAGE: '/api/message'
 };
 
-function setupModals () {
-  document.getElementById('postMessageButton').addEventListener('click', function (event) {
-    var messageInput = document.getElementById('messageInput');
-    var message = messageInput.value;
-
-    if (!message) {
-      alert('You must enter a message');
-      return;
-    }
-
-    __WEBPACK_IMPORTED_MODULE_0_axios___default.a.post(ENDPOINTS.MESSAGE, {
-      message: messageInput.value
-    })
+class ApiResource
+{
+  get() {
+    __WEBPACK_IMPORTED_MODULE_0_axios___default.a.get(ENDPOINTS.MESSAGE)
     .then(function (res) {
       console.log(res);
-
-      messageInput.value = '';
     })
     .catch(function (err) {
       console.log(err);
     });
-  });
-};
+  }
 
-
-
-
-/***/ }),
-/* 29 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (immutable) */ __webpack_exports__["a"] = clickNode;
-function clickNode(mesh) {
-  console.log('You clicked me');
-  mesh.material.color.setHex(0x0000ff);
+  post(message) {
+    __WEBPACK_IMPORTED_MODULE_0_axios___default.a.post(ENDPOINTS.MESSAGE, {
+      message: message
+    })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  }
 }
+/* harmony export (immutable) */ __webpack_exports__["a"] = ApiResource;
+
 
 
 /***/ })
