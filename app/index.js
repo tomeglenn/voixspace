6f60331bb6390e51760bfeb120bae76923f99bde
Tom Glenn <tom.glenn@stormid.com>
2017-04-06 16:03:32 +0100

    REACTgit sgit s

--- app/index.js
+++ app/index.js
@@ -1,55 +1,12 @@
-import ApiResource from './apiResource'
-import axios from 'axios';
 
-export class App {
-  constructor() {
-    this.apiResource = new ApiResource();
-
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-
-    this.camera.position.z = 5;
-  }
-
-  addCube() {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
-  }
-
-  render() {
-    requestAnimationFrame(this.render.bind(this));
-
-    this.cube.rotation.x = Date.now() * 0.0005;
-    this.cube.rotation.y = Date.now() * 0.0001;
-
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  addToDom() {
-    document.body.appendChild(this.renderer.domElement);
-  }
-
-  registerDomEvents() {
-    var domEvents = new THREEx.DomEvents(this.camera, this.renderer.domElement);
-    domEvents.addEventListener(this.cube, 'click', function (event) {
-      event.target.material.color.setHex(0x0000ff);
-      this.apiResource.get();
-    }.bind(this));
-  }
-
-  init() {
-    this.addCube();
-    this.addToDom();
-    this.registerDomEvents();
-    this.render();
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+import ThreeCanvas from './components/three-canvas';
 
 window.onload = function() {
-  var app = new App();
-  app.init();
+  ReactDOM.render(<App />, document.getElementById('app'));
+
+  var threeCanvas = new ThreeCanvas();
+  threeCanvas.init();
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
   entry: './app/index.js',
   output: {
     filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 };
